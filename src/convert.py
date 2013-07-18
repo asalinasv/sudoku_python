4c89154dcc39532c512bfabacb039e464ae409e3
oscartap <oscar.tapia@jalasoft.com>
2013-07-18 16:29:34 -0400

    R2_US02_Implement a Convert class Implement some methods to help the integration between classes inputs / outputs

--- src/convert.py
+++ src/convert.py
@@ -1,3 +1,8 @@
+# GeneralConverver class: auxiliar class that helps to return the desire input
+#                         for the required classes.
+# Author: Oscar Walker Tapia Merida - oscar.tapia@jalasoft.com
+# Automation Class (Sudoku project) - 2013
+
 import collections
 from readfiles import *
 
@@ -6,7 +11,8 @@ class GeneralConverter:
         pass
         
     def convert_txt_file_to_string(self,txt_file):
-        txtreader = ReadFiles(txt_file)
+        """ Convert a txt file into a string """
+        txtreader = SudokuFileReader(txt_file)
         buffertxt = txtreader.reading_txt()
         if(txtreader.validate_size_txt()==81 and txtreader.validate_values_txt()):
             string = ''
@@ -18,7 +24,8 @@ class GeneralConverter:
             return "Please insert a txt file with the correct dimensions"
 
     def convert_csv_file_to_string(self,csv_file):
-        csvreader = ReadFiles(csv_file)
+        """ Convert a csv file into a string """
+        csvreader = SudokuFileReader(csv_file)
         buffercsv = csvreader.reading_csv()
         if(csvreader.validate_size_csv()==81 and csvreader.validate_values_csv()):
             string = ''
@@ -32,6 +39,7 @@ class GeneralConverter:
 
 
     def convert_dictionary_to_string(self,dictionary):
+        """ Order and convert a dictionary into a string """
         ordered = collections.OrderedDict(sorted(dictionary.items()))
         string = ''
         for k, v in ordered.iteritems():
@@ -39,18 +47,19 @@ class GeneralConverter:
         return string
 
     def convert_txt_file_to_matrix(self,txt_file):
-        txtreader = ReadFiles(txt_file)
+        """ Convert a txt file to a matrix of integers """
+        txtreader = SudokuFileReader(txt_file)
         buffertxt = txtreader.reading_txt()
         M=[]
         for i in range(9):
             M.append([0]*9)
-        a, i, j = 0,0,0
+        i = 0
+        j = 0
         try:
             for x in buffertxt:
                 if(x != '\n'):
                     M[i][j] = int(x)
                     j += 1
-                    a += 1
                     if(j == 9):
                         i += 1
                         j = 0
@@ -60,15 +69,32 @@ class GeneralConverter:
         return M
 
     def convert_matrix_to_string(self,matrix):
+        """ Convert a matrix to a string  """
         string = ''
         for i in matrix:
             for j in i:
                 string += str(j)
         return string
 
+    def convert_string_to_matrix(self, string):
+        """ Convert a string to a matrix"""
+        M=[]
+        for i in range(9):
+            M.append([0]*9)
+        i = 0
+        j = 0
+        try:
+            for x in string:
+                M[i][j] = str(x)
+                j += 1
+                if(j == 9):
+                    i += 1
+                    j = 0
+        except IndexError:
+            print "PLEASE INSERT A 9x9 MATRIZ in a TXT file"
+            return False
+        return M
 
-prueba = GeneralConverter()
-#a = prueba.convert_txt_file_to_matrix("juego.txt")
-print prueba.convert_txt_file_to_matrix("juego.txt")
-#b = prueba.convert_matrix_to_string(a)
-#print b
+#con = GeneralConverter()
+#a = con.convert_string_to_matrix('483921657967345821251876493548132976729564138136798245372689514814253769695417382')
+#print a
