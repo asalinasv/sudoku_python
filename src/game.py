363216d70e6084485667e90b473d346d8070e404
Ana Salinas <ana.salinas@jalasoft.com>
2013-07-19 17:16:29 -0700

    R6_US01_Integration code Create SettingsConfiguratorDisplay submenu and SaveSetting class to integrate Configuration, ReadConfiguration and SettingStorer classes

--- src/game.py
+++ src/game.py
@@ -4,10 +4,15 @@ import cmd
 # Automation Class (Sudoku project) - 2013
 
 import os
+import sys
+sys.path.append('./src/Configuration')
+
 from solver import *
 from commandline import *
-from readconfiguration import *
+from readconfiguration import FileReader
 from storer import *
+from store_setting import StorerSetting
+from configuration import Configuration
 
 class Welcome:
     
@@ -41,9 +46,9 @@ class Menu:
         if value == 2:
             return CmdResolver().resolvefromcmd()
         if value == 3:
-            return SudokuGenerator().generatesudoku()
+            return SudokuGeneratorGame().generatesudoku()
         if value == 4:
-            return SettingsConfigurator().configuresettings()
+            return SettingsConfiguratorDisplay("config.ini").configuresettings()
         if value == 5:
             return Welcome().welcome()
         if value == 6:
@@ -81,21 +86,83 @@ class CmdResolver:
         cmd.solve_from_commandline()
         return
 
-class SudokuGenerator:
+class SudokuGeneratorGame:
 
     def generatesudoku(self):
         os.system('cls')
-        print "\n\n ********** Under construction ****************"
+        print "\n\
+               The 'Sudoku Game' will be generated based on the 'Difficult Level' set at configuration file "
         return
 
-class SettingsConfigurator:
 
+class SettingsConfiguratorDisplay:
+    legend = "\nThis is the current setting values of configuration file:\n"
+    setting_menu = "\nPlease select the option to modify any value or leave by default\n"
+    path = ("../Configuration/config.ini")
+    
+    read_file_setting = ""
+    def __init__(self, config_file):
+        self.read_file_setting = FileReader(self.path)
+                
     def configuresettings(self):
         os.system('cls')
-        print "*** Under COnstruction ***"
-        return
+        print self.legend
+        value_setting = self.read_file_setting.read_txt_file()
+        
+        for i in range(len(value_setting)):
+                       print value_setting[i]
+        self.sub_menu_setting()
+        
+    def sub_menu_setting(self):
+        
+        print self.setting_menu
+        print '\
+                1: Leave By default\n\
+                2: Modify Setting Values\n\
+                3: Back To Main Menu\n'
+        
+        value = int(raw_input())
+        if value == 1:
+            return Menu().menu()
+        
+        if value == 2:
+            return SaveSetting().save_all_setting_in_file(self.path)
+        
+        if value == 6:
+            return Menu().menu()
+        
 
+class SaveSetting:
+    
+    def save_setting_in_file(self, path):
+        os.system('cls')
+        print '\n\
+               Insert the new output file format Txt, or Csv\n'
+        value = int(raw_input()) 
+    
+    def save_all_setting_in_file(self, path):
+        os.system('cls')
+        
+        print 'Please Insert the Output_file:\n'
+        output_file = str(raw_input())
+              
+        print 'Please Insert the Default_alghoritm:\n'
+        default_algorithm = str(raw_input())
+        
+        print 'Please Insert the Dificult_level:\n'
+        difficult_level = str(raw_input())
+        
+        settings_dic = {"Output_file":output_file, "Default_alghoritm":default_algorithm, "Dificult_level":difficult_level}
+        
+        save_setting = StorerSetting(path, settings_dic)
+        
+        """if save_setting.save_txt_config_file() == "File Created":
+            print "File Created"
+            input("Press any key to come back Main menu")
+         """
+        return Menu().menu()
 
+    
 class Exit:
 
     def exit(self):
