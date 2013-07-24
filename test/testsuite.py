fddfd9aac36d00fe8ae061b11fa8a3f8f954a1c0
iazurduy <Israel Azurduy@IAZURDUYM-DH02.jalasoft.local>
2013-07-24 19:24:29 -0400

    Updated Test suite Added tests_algorithm, tests_playing, tests_sudoku, other were commented

--- test/testsuite.py
+++ test/testsuite.py
@@ -4,18 +4,24 @@ from coverage import coverage
 cov = coverage()
 cov.start()
 
-from readfiles_tests import TestReadFiles
-from norvig_tests import TestNorvigAlgorithm
-from solver_tests import TestSolver
-from convert_tests import TestConvert
+#from readfiles_tests import TestReadFiles
+#from norvig_tests import TestNorvigAlgorithm
+#from solver_tests import TestSolver
+#from convert_tests import TestConvert
+from tests_sudoku import TestBacktracking
+from tests_algorithm import TestAlgorithm
+from tests_playing import TestPlaying
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestReadFiles))
-    suite.addTest(unittest.makeSuite(TestNorvigAlgorithm))
-    suite.addTest(unittest.makeSuite(TestSolver))
-    suite.addTest(unittest.makeSuite(TestConvert))
-    
+    #suite.addTest(unittest.makeSuite(TestReadFiles))
+    #suite.addTest(unittest.makeSuite(TestNorvigAlgorithm))
+    #suite.addTest(unittest.makeSuite(TestSolver))
+    #suite.addTest(unittest.makeSuite(TestConvert))
+    suite.addTest(unittest.makeSuite(TestBacktracking))
+    suite.addTest(unittest.makeSuite(TestAlgorithm))
+    suite.addTest(unittest.makeSuite(TestPlaying))
+
     unittest.TextTestRunner(verbosity=4).run(suite)
 
     cov.stop()
